71ee15c2bc7ebad2f24bdcca4c254de137164a4c
Emelie <Emelie.almlof@gmail.com>
2022-11-08 22:30:40 +0100

    some changes to functions

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ let messageText = document.querySelector("#message")
 let toDoList = document.querySelector("#to-do-list");
 let removeToDo = document.querySelector("#remove-task");
 
-// Counter for number of To Do's - - - - - - - - - - - - - - - - 
-let numberOfToDo = 0;
-
-// Empty array for To Do's - - - - - - - - - - - - - - - - - - - - 
+// Empty array for To Do - - - - - - - - - - - - - - - - - - - - 
 let toDoArr = [];
 
 // Function add To Do - - - - - - - - - - - - - - - - - - - - - - 
@@ -22,7 +19,6 @@ function addToDo() {
         messageText.innerText = "Please add a new prompt"
         return false;
     } else {
-        numberOfToDo++;
         toDoArr.push(arrInput);
         toDoInput.value = "";
         displayToDo();
@@ -31,49 +27,35 @@ function addToDo() {
 
 // Function display To Do - - - - - - - - - - - - - - - - - - - - 
 function displayToDo() {
+
     let toDoLi = document.createElement("li");
 
-    toDoLi.innerText = toDoArr[numberOfToDo-1];
-    toDoLi.innerHTML += `<label> <input type="checkbox" value="${numberOfToDo}"></label>`
-    // console.log("value is: " + numberOfToDo);
+    toDoArr.forEach((toDo) => {
+        // toDoLi.innerHTML="";
+        toDoLi.innerText = toDo
+        toDoLi.innerHTML += `<label> <input type="checkbox" value="2"</label>`
+        // toDoLi.innerHTML += `<input type="checkbox" value="2"><label>${toDo}</label></input>`
+    })
     
     toDoList.append(toDoLi);
-    // console.log("list item: " + toDoArr[numberOfToDo-1])
-    // console.log(" number of todo: " + numberOfToDo);
 }
 
-// Cross out To Do - - - - - - - - - - - - - - - - - - - - - - - 
-
-    // Strike() ?? om man kryssar i checkboxar ska texten strykas över
-    // if checkbox ikryssad --> stryk över text
-    // addeventlisteners på checkboxar? 
-    // flytta checkboxar till framför li
-    
 // Function remove checkboxes - - - - - - - - - - - - - - - - - - 
 function removeCheckboxes() {
     
     let allCheckedBoxes = document.querySelectorAll("input[type='checkbox']:checked");
-    // console.log(allCheckedBoxes);
-    let doneToDo = [];
-    // console.log(doneToDo);
-    
+
     allCheckedBoxes.forEach((box) => {
-        doneToDo.push(box.value);
-        
-        console.log("doneTodo arr " + doneToDo);
-        //Värdet i arr skapas i checkboxarna och kommer från numberOfToDo 
-        //Värdena blir de checkboxar som är ikryssade, alltså ex. 1, 3, 4 osv.
-        //Hur ta bort listelement baserat på ikryssade checboxar?
+        box.parentElement.parentElement.remove();
+        toDoArr.splice(parseInt(box.value)-1, 1);
     });
 }
         
-// Buttons - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// Buttons and keys - - - - - - - - - - - - - - - - - - - - - - - - -
 addBtn.addEventListener("click", () =>{
     addToDo();
 });
 
-
-// Enter key - - - - - - - - - - - - - - - - - - - - - - - - - - -
 toDoInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         addToDo();
